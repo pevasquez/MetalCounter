d8aad5dbe0e37e5d3e619b54849cd0bc2b1378f2
Pablo Vasquez <pablo.vasquez@etermax.com>
2017-06-25 16:33:14 -0300

    Initial Commit

---
+++ MetalCounter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MetalCounter
+//
+//  Created by Pablo on 6/25/17.
+//  Copyright © 2017 pevasquez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
